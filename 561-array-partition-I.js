93e0dddeaa2c3f9aeece740a055fcb804e0f333c
heegu.park <heegu@withving.com>
2021-03-21 16:47:46 -0700

    Updated

--- 561-array-partition-I.js
+++ 561-array-partition-I.js
@@ -21,8 +21,6 @@
 var arrayPairSum = function(nums) {
     nums.sort((a,b)=>a-b);
     let sum = 0;
-    for(let i=0;i<nums.length;i=i+2) {
-        sum += nums[i];
-    }
+    for(let i=0;i<nums.length;i=i+2) sum += nums[i];
     return sum;
 };
