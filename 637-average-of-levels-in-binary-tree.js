4c7e1066d56b9799eedc3f4a821e699c831c66f2
heegu.park <slhead@gmail.com>
2021-03-23 15:16:08 -0700

    Updated code

--- 637-average-of-levels-in-binary-tree.js
+++ 637-average-of-levels-in-binary-tree.js
@@ -27,8 +27,7 @@ var averageOfLevels = function(root) {
     let result = [], currentLevel = [];
     currentLevel.push(root);
     while(currentLevel.length > 0) {
-        let num = currentLevel.length, sum = 0;
-        let i = num;
+        let num = currentLevel.length, sum = 0, i = num;
         while(i > 0){
             let node = currentLevel.shift();
             sum += node.val;
