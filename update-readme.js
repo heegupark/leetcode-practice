26c4b3ea53e0eb3ff26aa5e35eb65ee98d36b117
heegu.park <slhead@gmail.com>
2021-05-15 20:50:54 -0700

    Added

--- update-readme.js
+++ update-readme.js
@@ -6,15 +6,34 @@ const folder = './';
 const outputFileName = 'test.md';
 const execSync = require('child_process').execSync;
 
-if(fs.existsSync(`${folder}${outputFileName}`)) fs.unlinkSync(`${folder}${outputFileName}`);
+const getNewestFile = (files, path) => {
+    var out = [];
+    files.forEach(function(file) {
+        var stats = fs.statSync(path + "/" +file);
+        if(stats.isFile()) {
+            out.push({"file":file, "mtime": stats.mtime.getTime()});
+        }
+    });
+    out.sort(function(a,b) {
+        return b.mtime - a.mtime;
+    })
+    return (out.length>0) ? out[0].file : "";
+}
 
+const getTitle = (title) => {
+  return title.split('-').map((item) => item[0].toUpperCase() + item.substring(1))
+};
+
+if(fs.existsSync(`${folder}${outputFileName}`)) fs.unlinkSync(`${folder}${outputFileName}`);
+let mostRecentFile = '';
 fs.readdir(folder, (err, files) => {
+  mostRecentFile = getNewestFile(files, folder);
   let stream = fs.createWriteStream(outputFileName, {'flags': 'a'});
   stream.once('open', (fd) => {
     stream.write("# leetcode-practice(javascript)" + "\r\n");
     stream.write("\r\n" + "Leetcode problem practice(JavaScript)" + "\r\n");
     files.forEach(file => {
-      const tempTitle = file.split('-').map((item) => item[0].toUpperCase() + item.substring(1));
+      const tempTitle = getTitle(file);
       if(Number.isInteger(Number(file.split('-')[0]))) {
         const title = `${tempTitle.shift()}. ${tempTitle.join(' ')}`;
         const line = `### <a href="https://github.com/heegupark/leetcode-practice/blob/main/${file}.js">${title}</a>`;
@@ -24,20 +43,9 @@ fs.readdir(folder, (err, files) => {
   });
 });
 
-// const name = process.argv[2];
-const add = execSync('git add .', { encoding: 'utf-8' });  // the default is 'buffer'
-const commit = execSync('git commit -m "Updated"', { encoding: 'utf-8' });  // the default is 'buffer'
-const push = execSync('git push origin main', { encoding: 'utf-8' });  // the default is 'buffer'
-console.log('git add', add)
-console.log('git commit', commit)
-console.log('git push', push)
-// exec('git add .',
-//     function (error, stdout, stderr) {
-//         console.log('stdout: ' + stdout);
-//         console.log('stderr: ' + stderr);
-//         if (error !== null) {
-//              console.log('exec error: ' + error);
-//         }
-//     });
-// exec('git commit -m "Updated"');
-// exec('git push origin main');
+const add = execSync('git add .', { encoding: 'utf-8' });
+const commit = execSync(`git commit -m "Added ${mostRecentFile}"`, { encoding: 'utf-8' });
+const push = execSync('git push origin main', { encoding: 'utf-8' });
+console.log('git add', add);
+console.log('git commit', commit);
+console.log('git push', push);
