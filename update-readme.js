ecd9e03bff72781650432522f15f050eadd4569d
heegu.park <slhead@gmail.com>
2021-05-15 20:33:11 -0700

    Updated

--- update-readme.js
+++ update-readme.js
@@ -1,10 +1,30 @@
 const fs = require('fs');
 //1668-maximum-repeating-substring
-let stream = fs.createWriteStream("README.md", {'flags': 'a'});
-const name = process.argv[2];
-const tempTitle = name.split('-').map((item) => item[0].toUpperCase() + item.substring(1));
-const title = `${tempTitle.shift()}. ${tempTitle.join(' ')}`;
-const line = `### <a href="https://github.com/heegupark/leetcode-practice/blob/main/${name}.js">${title}</a>`;
-stream.once('open', (fd) => {
-  stream.write("\r\n" + line + "\r\n");
+// require('events').EventEmitter.defaultMaxListeners = 0;
+
+const folder = './';
+const outputFileName = 'test.md';
+const exec = require('child_process').exec;
+
+fs.unlinkSync(`${folder}${outputFileName}`);
+
+fs.readdir(folder, (err, files) => {
+  let stream = fs.createWriteStream(outputFileName, {'flags': 'a'});
+  stream.once('open', (fd) => {
+    stream.write("# leetcode-practice(javascript)" + "\r\n");
+    stream.write("\r\n" + "Leetcode problem practice(JavaScript)" + "\r\n");
+    files.forEach(file => {
+      const tempTitle = file.split('-').map((item) => item[0].toUpperCase() + item.substring(1));
+      if(Number.isInteger(Number(file.split('-')[0]))) {
+        const title = `${tempTitle.shift()}. ${tempTitle.join(' ')}`;
+        const line = `### <a href="https://github.com/heegupark/leetcode-practice/blob/main/${file}.js">${title}</a>`;
+        stream.write("\r\n" + line + "\r\n");
+      }
+    });
+  });
 });
+
+// const name = process.argv[2];
+exec('git add .');
+exec('git commit -m "Updated"');
+exec('git push origin main');
