8ca35485285d54517182827ab4385ccb0ce30e36
heegu.park <slhead@gmail.com>
2021-06-09 08:22:54 -0700

    Added 1784 Check If Binary String Has At Most One Segment Of Ones.js

---
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones.js
@@ -0,0 +1,24 @@
+// 1784. Check if Binary String Has at Most One Segment of Ones
+
+// Given a binary string s ​​​​​without leading zeros, return true​​​ if s contains at most one contiguous segment of ones. Otherwise, return false.
+// Example 1:
+// Input: s = "1001"
+// Output: false
+// Explanation: The ones do not form a contiguous segment.
+
+// Example 2:
+// Input: s = "110"
+// Output: true
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkOnesSegment = function(s) {
+    let flag =  false;
+    for(let i = 0 ; i< s.length ; i++) {
+        if(s[i] === "0") flag = true;
+        if(flag && s[i] === "1") return false;
+    }
+    return true;
+};
--- README.md
+++ README.md
@@ -148,6 +148,8 @@ Leetcode problem practice(JavaScript)
 
 ### <a href="https://github.com/heegupark/leetcode-practice/blob/main/1748-sum-of-unique-elements.js">1748. Sum Of Unique Elements.js</a>
 
+### <a href="https://github.com/heegupark/leetcode-practice/blob/main/1784-check-if-binary-string-has-at-most-one-segment-of-ones.js">1784. Check If Binary String Has At Most One Segment Of Ones.js</a>
+
 ### <a href="https://github.com/heegupark/leetcode-practice/blob/main/20-valid-parentheses.js">20. Valid Parentheses.js</a>
 
 ### <a href="https://github.com/heegupark/leetcode-practice/blob/main/219-contains-duplicate-II.js">219. Contains Duplicate II.js</a>
