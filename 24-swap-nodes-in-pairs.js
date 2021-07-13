3748bc89c89e2bf320e06ad65e413df7385794fd
heegupark <slhead@gmail.com>
2021-07-13 15:07:36 -0700

    Added 24 Swap Nodes In Pairs.js

--- 24-swap-nodes-in-pairs.js
+++ 24-swap-nodes-in-pairs.js
@@ -1,5 +1,6 @@
-// Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+// 24 Swap Nodes in Pairs
 
+// Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
 // Example 1:
 // Input: head = [1,2,3,4]
 // Output: [2,1,4,3]
