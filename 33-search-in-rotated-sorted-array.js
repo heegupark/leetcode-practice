f0f03497c3afac3a9f46c26dc8fd0e61655a0ef3
heegupark <slhead@gmail.com>
2021-07-15 08:16:41 -0700

    Added 33 Search In Rotated Sorted Array.js

--- 33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array.js
@@ -21,24 +21,23 @@
  * @param {number} target
  * @return {number}
  */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
 var search = function(nums, target) {
-    if (nums.length === 0) return -1; // check empty
+    if (nums.length === 0) return -1;
     let left = 0, right = nums.length - 1;
     while (left <= right) {
         const mid = left + Math.floor((right - left) / 2);
         if(nums[mid] === target) return mid;
         if (nums[left] <= nums[mid]) {
-            if (nums[left] <= target && target < nums[mid]) {
-              right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
+            if (nums[left] <= target && target < nums[mid]) right = mid - 1;
+            else left = mid + 1;
         } else {
-            if (nums[mid] < target && target <= nums[right]) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+            if (nums[mid] < target && target <= nums[right]) left = mid + 1;
+            else right = mid - 1;
         }
     }
     return -1;
