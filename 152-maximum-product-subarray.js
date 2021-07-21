b4827b9c84fea4e1177942ef31e09dd146161999
heegupark <slhead@gmail.com>
2021-07-21 07:39:56 -0700

    Added 152 Maximum Product Subarray.js

--- 152-maximum-product-subarray.js
+++ 152-maximum-product-subarray.js
@@ -2,7 +2,6 @@
 // Given an integer array nums, find a contiguous non-empty subarray within the array that has the largest product, and return the product.
 // It is guaranteed that the answer will fit in a 32-bit integer.
 // A subarray is a contiguous subsequence of the array.
-
 // Example 1:
 // Input: nums = [2,3,-2,4]
 // Output: 6
