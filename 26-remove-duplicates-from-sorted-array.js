a454b5ee7a76201104c1bcba2a42d9c7bfbf0c6a
heegupark <slhead@gmail.com>
2021-08-05 10:07:49 -0700

    Added 26 Remove Duplicates From Sorted Array.js

--- 26-remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array.js
@@ -36,3 +36,15 @@ var removeDuplicates = function(nums) {
     }
     return nums.length;
 };
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let i=0;
+    while(i<nums.length) {
+        if (nums[i] === nums[i+1]) nums.splice(i,1);
+        else i++;
+    }
+    return nums.length;
+};
