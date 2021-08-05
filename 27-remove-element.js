a93978de33cae65dc9cc2b538d8e2dfeebf89388
heegupark <slhead@gmail.com>
2021-08-05 14:27:01 -0700

    Added 27 Remove Element.js

--- 27-remove-element.js
+++ 27-remove-element.js
@@ -32,5 +32,5 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    for(let i = nums.length - 1; i >= 0; i--) if(nums[i] === val) nums.splice(i,1)
+    for(let i = nums.length - 1; i >= 0; i--) if(nums[i] === val) nums.splice(i,1);
 };
