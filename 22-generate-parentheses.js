d186620eea03983872950787111f09a7014bedcf
heegupark <slhead@gmail.com>
2021-08-08 15:28:19 -0700

    Added 22 Generate Parentheses.js

--- 22-generate-parentheses.js
+++ 22-generate-parentheses.js
@@ -1,9 +1,11 @@
 // 22. Generate Parentheses
 
 // Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
 // Example 1:
 // Input: n = 3
 // Output: ["((()))","(()())","(())()","()(())","()()()"]
+
 // Example 2:
 // Input: n = 1
 // Output: ["()"]
