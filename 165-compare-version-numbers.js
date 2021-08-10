e4784a47bdb90830e20bb39a377bc327084c582f
heegupark <slhead@gmail.com>
2021-08-10 07:44:28 -0700

    Added 165 Compare Version Numbers.js

--- 165-compare-version-numbers.js
+++ 165-compare-version-numbers.js
@@ -7,21 +7,26 @@
 // If version1 < version2, return -1.
 // If version1 > version2, return 1.
 // Otherwise, return 0.
+
 // Example 1:
 // Input: version1 = "1.01", version2 = "1.001"
 // Output: 0
 // Explanation: Ignoring leading zeroes, both "01" and "001" represent the same integer "1".
+
 // Example 2:
 // Input: version1 = "1.0", version2 = "1.0.0"
 // Output: 0
 // Explanation: version1 does not specify revision 2, which means it is treated as "0".
+
 // Example 3:
 // Input: version1 = "0.1", version2 = "1.1"
 // Output: -1
 // Explanation: version1's revision 0 is "0", while version2's revision 0 is "1". 0 < 1, so version1 < version2.
+
 // Example 4:
 // Input: version1 = "1.0.1", version2 = "1"
 // Output: 1
+
 // Example 5:
 // Input: version1 = "7.5.2.4", version2 = "7.5.3"
 // Output: -1
