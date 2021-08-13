e96ea99b8b854511566bafc7a754687add44602a
heegupark <slhead@gmail.com>
2021-08-13 11:46:20 -0700

    Added 14 Longest Common Prefix.js

--- 14-longest-common-prefix.js
+++ 14-longest-common-prefix.js
@@ -2,9 +2,11 @@
 
 // Write a function to find the longest common prefix string amongst an array of strings.
 // If there is no common prefix, return an empty string "".
+
 // Example 1:
 // Input: strs = ["flower","flow","flight"]
 // Output: "fl"
+
 // Example 2:
 // Input: strs = ["dog","racecar","car"]
 // Output: ""
