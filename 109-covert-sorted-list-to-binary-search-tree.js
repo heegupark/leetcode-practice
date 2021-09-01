b8d83cb6b9da3eadc106b75bf36cd9dfd4a0af12
heegupark <slhead@gmail.com>
2021-09-01 08:48:55 -0700

    Added 109 Covert Sorted List To Binary Search Tree.js

--- 109-covert-sorted-list-to-binary-search-tree.js
+++ 109-covert-sorted-list-to-binary-search-tree.js
@@ -1,20 +1,25 @@
 // 109. Convert Sorted List to Binary Search Tree
+
 // Given the head of a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
 // For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
 // Example 1:
 // Input: head = [-10,-3,0,5,9]
 // Output: [0,-3,9,-10,null,5]
 // Explanation: One possible answer is [0,-3,9,-10,null,5], which represents the shown height balanced BST.
+
 // Example 2:
 // Input: head = []
 // Output: []
+
 // Example 3:
 // Input: head = [0]
 // Output: [0]
-// Example 4:
 
+// Example 4:
 // Input: head = [1,3]
 // Output: [3,1]
+
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
