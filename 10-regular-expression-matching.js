511e7ce86e6793e79f75b8e4f96588205068b187
heegupark <slhead@gmail.com>
2022-04-23 20:22:03 -0700

    10 Regular Expression Matching

---
+++ 10-regular-expression-matching.js
@@ -0,0 +1,42 @@
+// Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+
+// '.' Matches any single character.​​​​
+// '*' Matches zero or more of the preceding element.
+// The matching should cover the entire input string (not partial).
+
+// Example 1:
+// Input: s = "aa", p = "a"
+// Output: false
+// Explanation: "a" does not match the entire string "aa".
+
+// Example 2:
+// Input: s = "aa", p = "a*"
+// Output: true
+// Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+
+// Example 3:
+// Input: s = "ab", p = ".*"
+// Output: true
+// Explanation: ".*" means "zero or more (*) of any character (.)".
+
+// Constraints:
+// 1 <= s.length <= 20
+// 1 <= p.length <= 30
+// s contains only lowercase English letters.
+// p contains only lowercase English letters, '.', and '*'.
+// It is guaranteed for each appearance of the character '*', there will be a previous valid character to match.
+
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch = function(s, p) {
+  if (!p.length) return s.length === 0;
+  const firstMatch = s.length > 0 && (s[0] === p[0] || p[0] === ".");
+  if (p.length >= 2 && p[1] === "*") {
+    return isMatch(s, p.substring(2)) || (firstMatch && isMatch(s.substring(1), p))
+  } else {
+    return firstMatch && isMatch(s.substring(1), p.substring(1));
+  }
+};
